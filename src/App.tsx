49ed17aae1c4352a5fc9232073664ec56db45725
Gelzieny R. Martins <gelzieny@gmail.com>
2026-04-22 20:48:51 -0300

    Estilizando Main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import styles from "./app.module.css";
 import { Header } from "./components/Header";
+import { Letter } from "./components/Letter";
 import { Tip } from "./components/Tip";
 
 export function App() {
@@ -12,6 +13,14 @@ export function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
         <Tip tip="Uma das linguagens de programação mais utilizadas" />
+
+        <div className={styles.word}>
+          <Letter value="R" />
+          <Letter value="E" />
+          <Letter value="A" />
+          <Letter value="C" />
+          <Letter value="T" />
+        </div>        
       </main>
     </div>
   );
