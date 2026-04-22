678c23d3af7c834ea3f6a7f948d7f81682cfc4ce
Gelzieny R. Martins <gelzieny@gmail.com>
2026-04-22 20:45:29 -0300

    Componente de Dica

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import styles from "./app.module.css";
 import { Header } from "./components/Header";
+import { Tip } from "./components/Tip";
 
-export default function App() {
+export function App() {
   function handleRestartGame() {
     alert("Reiniciar o jogo!");
   }
@@ -10,6 +11,7 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Uma das linguagens de programação mais utilizadas" />
       </main>
     </div>
   );
